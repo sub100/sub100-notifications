44399c22cb23a69bd512921483b07c8018850607
André Silva <dev.andregnx@gmail.com>
2020-10-05 17:59:28 -0300

    Add Notification Facade public methods docblock

--- src/Facades/Notification.php
+++ src/Facades/Notification.php
@@ -3,7 +3,11 @@
 namespace sub100\Notifications\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use sub100\Notifications\Message;
 
+/**
+ * @method bool notify(Message $message, string $token = '')
+ */
 class Notification extends Facade
 {
     protected static function getFacadeAccessor()
